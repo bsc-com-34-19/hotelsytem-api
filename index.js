b2f03db3dd508c0e4c2d5a9a5359534deadbacd2
bsc-com-34-19 <bsc-com-34-19@unima.ac.mw>
2023-04-07 11:46:44 +0200

    fix:circular error when req object returned

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+const { request } = require('express')
 const express = require('express')
 const app = express()
 
-const post = require('./src/posts/posts.model ')
+const post = require('./src/posts/posts.model')
 
-app.get('/', function(req, res){
-    return res.json(req)
+app.get('/api/v1', function(req, res){
+    return res.json(req.headers)
 })
+
 app.get('/api/v1/posts', function(req, res){
-    return res.json(req)
+    return res.json([post])
 })
+
 app.listen(3000, function() {
     console.log('Hotel system  listening on port 3000');
 })
