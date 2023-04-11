ca78fdee452c397cff1ea7caa108e3b0ced203e7
bsc-com-34-19 <bsc-com-34-19@unima.ac.mw>
2023-04-11 09:51:18 +0200

    Another commit after database created

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const { request } = require('express')
 const express = require('express')
 const app = express()
 
+const dbConnection = require('./src/utils/mysql.connector')
+
+
 const post = require('./src/posts/posts.model')
 
 app.get('/api/v1', function(req, res){
@@ -14,4 +17,9 @@ app.get('/api/v1/posts', function(req, res){
 
 app.listen(3000, function() {
     console.log('Hotel system  listening on port 3000');
+
+    dbConnection.connect(function (err){
+        if (err) throw err
+        console.log("Connected to Mysql")
+    } )
 })
---
+++ src/utils/mysql.connector.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const options = {
+      host: "localhost",
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    connectionLimit: 100,
+}
+
+const connection = mysql.createConnection(config)
+
+module.exports = connection
